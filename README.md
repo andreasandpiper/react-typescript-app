847776ea76684fa1f4b942f6ece244a3e48f724a
Andrea Wayte <andrea.wayte@gmail.com>
2021-08-06 15:03:39 -0700

    initial commit, basic js project with jest

---
+++ README.md
@@ -0,0 +1,17 @@
+# React Typescript App
+
+## Getting started
+
+1. `npm install` to install local dependencies
+
+### Run locally
+
+- `npm run dev`
+
+### Tun tests
+
+- `npm test`
+
+### Check types
+
+- `npm run check:types`
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.tsx', '.jsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: '[name].js',
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      inject: 'body',
+    }),
+  ],
+  devServer: {
+    port: 9000,
+    open: true,
+    hot: true,
+    historyApiFallback: true,
+    client: {
+      overlay: {
+        errors: true,
+        warnings: false,
+      },
+    },
+  },
+};
